10fc863b7d963589f7fb6aa46edb65b1677b758c
Diego Loyola <dieegolr@gmail.com>
2020-01-20 01:40:51 -0300

    Updates

---
+++ precache-manifest.96cd57929b02ddf9a41cb1af7aaf1b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471c6a59bd4dab285ff8ce26653d0f86",
+    "url": "/control-de-gastos/index.html"
+  },
+  {
+    "revision": "b56e7093dda06cef7cd4",
+    "url": "/control-de-gastos/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "695de977778667aae37f",
+    "url": "/control-de-gastos/static/js/2.5bb3008c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/control-de-gastos/static/js/2.5bb3008c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b56e7093dda06cef7cd4",
+    "url": "/control-de-gastos/static/js/main.137498a6.chunk.js"
+  },
+  {
+    "revision": "e337140c697d0c279f71",
+    "url": "/control-de-gastos/static/js/runtime-main.385ccf36.js"
+  }
+]);
